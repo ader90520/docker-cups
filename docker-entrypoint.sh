001fa8c5865aa09f4e6e65bf4f3ef8e7c53cf18c
ader <80502911+ader90520@users.noreply.github.com>
2026-09-10 12:28:19 +0800

    更新 docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,18 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
-echo -e "${ADMIN_PASSWORD}\n${ADMIN_PASSWORD}" | passwd admin
-
+# 如果没有配置文件，则从骨架目录拷贝
 if [ ! -f /etc/cups/cupsd.conf ]; then
-  cp -rpn /etc/cups-skel/* /etc/cups/
+    cp -r /etc/cups-skel/* /etc/cups/
 fi
 
+# 设置 admin 密码
+if [ -n "$ADMIN_PASSWORD" ]; then
+    echo "admin:$ADMIN_PASSWORD" | chpasswd
+fi
+
+# 在后台启动邮件云打印监听（不阻塞主进程）
+python3 /usr/local/bin/mail_print.py &
+
+# 执行原定启动命令 (cupsd -f)
 exec "$@"
